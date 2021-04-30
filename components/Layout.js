4c38d0698f61d6700b0512bf0b9b759d790d053a
Vishnu <vishnu007jr@gmail.com>
2021-04-30 11:40:53 +0530

    layout-completed

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Layout.module.css'
+import {useRouter} from 'next/router'
+import styles from '@/styles/Layout.module.css'
 import Footer from './Footer'
 import Header from './Header'
+import Showcase from './Showcase'
+
 
 export default function Layout({title,keywords,
 description,children}) {
+
+    const router=useRouter()
     return (
         <div>
             <Head>
@@ -13,6 +18,7 @@ description,children}) {
                 <meta name='keywords' content={keywords} />
             </Head>
             <Header />
+            {router.pathname==='/' && <Showcase />}
             <div className={styles.container}>
             {children}
             </div>
